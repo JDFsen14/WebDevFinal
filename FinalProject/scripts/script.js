41a9d44063f3fcd064df5dbb0cec7450ae42f6d6
Jaren Frandsen <31313517+JDFsen14@users.noreply.github.com>
2023-04-24 13:14:47 -0600

    Adding JS

---
+++ FinalProject/scripts/script.js
@@ -0,0 +1,62 @@
+var myNewImage = document.getElementById("newImage");
+var newHeader = document.getElementById("newHeader");
+var newText1 = document.getElementById("newContent1");
+var newText2 = document.getElementById("newContent2");
+var newText3 = document.getElementById("newContent3");
+
+function NES() {
+    newHeader.textContent = "The Nintendo Entertainment System";
+    newHeader.style.fontWeight = "bold";
+
+    newText1.textContent = "The Nintendo Entertainment System (NES) is an 8-bit third-generation home video game console produced by Nintendo. It was first released in Japan in 1983 as the Family Computer (FC), commonly known as the Famicom. The NES, a redesigned version, was released in American test markets on October 18, 1985, before becoming widely available in North America and other countries.";
+    newText1.style.fontsize = "2em";
+
+    newText2.textContent = "The console's standard display resolution is 256 by 240 pixels, though video output options vary between models. The original Famicom features only radio frequency (RF) modulator output, while the NES additionally includes support for composite video via RCA connectors. The redesigned Famicom omits the RF modulator entirely, only outputting composite video via a proprietary multi-out connector first introduced on the Super Famicom/NES; conversely, the redesigned NES features RF modulator output only, though a version of the model including the multi-out connector was produced in rare quantities.";
+    newText2.style.fontsize = "2em";
+
+    newText3.textContent = "The NES was one of the best-selling consoles of its time and helped revitalize the US gaming industry following the video game crash of 1983. It introduced a now-standard business model of licensing third-party developers to produce and distribute games. The NES featured a number of groundbreaking games, such as the 1985 platform game Super Mario Bros. and the 1986 action-adventure games The Legend of Zelda and Metroid, which became long-running franchises. It was succeeded in 1990 by the Super Nintendo Entertainment System. In 2011, IGN named the NES the greatest video game console of all time.";
+    newText3.style.fontsize = "2em";
+}
+
+function SNES() {
+    newHeader.textContent = "The Super NES";
+    newHeader.style.fontWeight = "bold";
+
+    newText1.textContent = "The Super Nintendo Entertainment System (SNES), commonly shortened to Super NES or Super Nintendo, is a 16-bit home video game console developed by Nintendo that was released in 1990 in Japan and South Korea, 1991 in North America, 1992 in Europe and Oceania, and 1993 in South America. In Japan, it is called the Super Famicom (SFC). In South Korea, it is called the Super Comboy and was distributed by Hyundai Electronics. The system was released in Brazil on August 30, 1993, by Playtronic. Although each version is essentially the same, several forms of regional lockout prevent cartridges for one version from being used in other versions.";
+    newText1.style.fontsize = "2em";
+
+    newText2.textContent = "The Super NES is Nintendo's second home video gaming console, following the Nintendo Entertainment System (NES). The console introduced advanced graphics and sound capabilities compared with other systems at the time. It was designed to accommodate the ongoing development of a variety of enhancement chips integrated into game cartridges to be competitive into the next generation";
+    newText2.style.fontsize = "2em";
+
+    newText3.textContent = "The Super NES received largely positive reviews and was a global success, becoming the bestselling console of the 16-bit era after launching relatively late and facing intense competition from Sega's Genesis console in North America and Europe. Overlapping the NES's 61.9 million unit sales, the Super NES remained popular well into the 32-bit era, with 49.1 million units sold worldwide by the time it was discontinued in 2003. It continues to be popular among collectors and retro gamers, with new homebrew games and Nintendo's emulated rereleases, such as on the Virtual Console, the Super NES Classic Edition, Nintendo Switch Online; as well as several non-console emulators which operate on a desktop computer, such as Snes9x.";
+    newText3.style.fontsize = "2em";
+}
+
+function N64() {
+    newHeader.textContent = "The Nintendo 64";
+    newHeader.style.fontWeight = "bold";
+
+    newText1.textContent = "The Nintendo 64 (N64) is a home video game console developed by Nintendo. The successor to the Super Nintendo Entertainment System, it was released on June 23, 1996 in Japan; September 26, 1996 in North America; and on March 1, 1997 in Europe and Australia. It was the last major home console to use cartridges as its primary storage format until the Nintendo Switch in 2017. It competed primarily with the Sony PlayStation and the Sega Saturn.";
+    newText1.style.fontsize = "2em";
+
+    newText2.textContent = "Development began in 1993 in partnership with Silicon Graphics, using the codename Project Reality, then a test model and arcade platform called Ultra 64. The final design was named after its 64-bit CPU, which aided in the console's 3D capabilities. Its design was mostly complete by mid-1995 and launch was delayed until 1996 for the completion of the launch games Super Mario 64, Pilotwings 64, and Saikyō Habu Shōgi (exclusive to Japan). The charcoal-gray console was followed by a series of color variants. Some games require the Expansion Pak accessory to increase system RAM from 4MB to 8MB, for improved graphics and functionality. The console mainly supports saved game storage either onboard cartridges or on the Controller Pak accessory. The 64DD peripheral drive hosts both exclusive games and expansion content for cartridges, with many further accessories plus the defunct Internet service Randnet, but it was a commercial failure and was released only in Japan";
+    newText2.style.fontsize = "2em";
+
+    newText3.textContent = "Time named it Machine of the Year in 1996, and in 2011, IGN named it the ninth-greatest video game console of all time. The Nintendo 64 was discontinued in 2002 following the 2001 launch of its successor, the GameCube. The Nintendo 64 was critically acclaimed and remains one of the most recognized video game consoles.";
+    newText3.style.fontsize = "2em";
+}
+
+function Gamecube() {
+    newHeader.textContent = "The Gamecube";
+    newHeader.style.fontWeight = "bold";
+
+    newText1.textContent = "The GameCube is a home video game console developed and released by Nintendo in Japan on September 14, 2001, in North America on November 18, 2001, and in PAL territories in 2002. It is the successor to the Nintendo 64 (1996), and predecessor of the Wii (2006). In the sixth generation of video game consoles, the GameCube competed with Sony's PlayStation 2 and Microsoft's Xbox. Flagship games include Super Smash Bros. Melee, Luigi's Mansion, Super Mario Sunshine, Metroid Prime, Mario Kart: Double Dash, Pikmin, Pikmin 2, The Legend of Zelda: The Wind Waker, Chibi-Robo!, and the original Animal Crossing.";
+    newText1.style.fontsize = "2em";
+
+    newText2.textContent = "Development was enabled by the 1997 formation of computer graphics company ArtX, of former SGI employees who had created the Nintendo 64, and which was later acquired by ATI to produce the GameCube's GPU. In May 1999, Nintendo announced codename Dolphin, released in 2001 as the GameCube. It is Nintendo's first console to use optical discs instead of ROM cartridges, supplemented by writable memory cards for saved games. Unlike its competitors, it is solely focused on gaming and does not play mass media like DVD or CD. The console supports limited online gaming for a few games via a GameCube broadband or modem adapter and can connect to a Game Boy Advance with a link cable for exclusive in-game features using the handheld as a second screen and controller. The GameCube supports e-Reader cards to unlock special features in a few games. The Game Boy Player add-on runs Game Boy, Game Boy Color, and Game Boy Advance cartridge games.";
+    newText2.style.fontsize = "2em";
+
+    newText3.textContent = "Reception of the GameCube was mixed. It was praised for its controller and extensive library of high-quality games, but was criticized for its exterior design and lack of multimedia features. Nintendo sold 21.74 million GameCube units worldwide, much fewer than anticipated, and discontinued it in 2007. It was succeeded by the Wii; the first model launched in November 2006 with full backward compatibility with GameCube games and accessories. Retrospective reception of the GameCube has been positive, emphasizing its excellent library; by early 2023, the two highest rated games of the year were both GameCube games, which caused a widescale reassessment of the console's legacy, recontextualizing it in a more positive light.";
+    newText3.style.fontsize = "2em";
+}
+    
